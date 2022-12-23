4b6eb4d974c94e28712845496ce63a6a0ae6b51e
tomatdevoteam <tom.ganemo@devoteam.com>
2022-12-23 16:42:18 +0100

    added shell stuff

---
+++ src/main/java/com/example/bookbackendexample/Controllers/BasicController.java
@@ -0,0 +1,21 @@
+package com.example.bookbackendexample.Controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class BasicController {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/main/java/com/example/bookbackendexample/Services/BasicService.java
@@ -0,0 +1,4 @@
+package com.example.bookbackendexample.Services;
+
+public class BasicService {
+}

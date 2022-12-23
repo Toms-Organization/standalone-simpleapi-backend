6be13f590005164a5a8ad39b787e5935b5235e12
tomatdevoteam <tom.ganemo@devoteam.com>
2022-12-23 16:29:27 +0100

    Initial commit

---
+++ src/main/java/com/example/bookbackendexample/BookBackendExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookbackendexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBackendExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookBackendExampleApplication.class, args);
+    }
+
+}

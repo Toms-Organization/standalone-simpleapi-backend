18d776e80b4d8a9ae3d66e04f2aaf133aefefc17
TomIsLearningJava <tom.ganemo1987@gmail.com>
2022-12-28 11:52:31 +0100

    added SpringFox and hibernate validation dependencies along with changing back to an older version of SpringBoot in order to get it to work.. However i will try another way but i wanna store this

--- src/main/java/com/example/bookbackendexample/BookBackendExampleApplication.java
+++ src/main/java/com/example/bookbackendexample/BookBackendExampleApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @SpringBootApplication(scanBasePackages = "com.example.bookbackendexample")
 public class BookBackendExampleApplication {
+
 
 
     public static void main(String[] args) {
--- src/main/java/com/example/bookbackendexample/models/Book.java
+++ src/main/java/com/example/bookbackendexample/models/Book.java
@@ -1,13 +1,14 @@
 package com.example.bookbackendexample.models;
 
 
-import jakarta.persistence.*;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-import java.util.Date;
+
 
 
 @Entity
